e66a86b385354985bc8597dedba8c49b8c8e9b5a
Juha Kauppinen <juha.kauppinen@helsinki.fi>
2023-01-24 13:19:52 +0200

    add react router and create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { DataGridForJourneys, DataGridForStations } from './components/DataGrid';
-import './index.css'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from "react-router-dom";
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <DataGridForJourneys />
-      <DataGridForStations />
-    </div>
-  );
+    <Router>
+      <h1>City Bike Journeys</h1>
+      <div>
+        <Link to="/">Journeys</Link>
+        <Link to="/stations">Stations</Link>
+        {/* <Link to="/maps">Maps</Link> */}
+      </div>
+
+      <Routes>
+        <Route path="/" element={<DataGridForJourneys />} />
+        <Route path="/stations" element={<DataGridForStations />} />
+        {/* <Route path="/maps" element={<DataGridForStations />} /> */}
+      </Routes>
+      <div>
+        <i>Note app, Department of Computer Science 2022</i>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
