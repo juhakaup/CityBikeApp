f84838738f96d7c04643e2c87507bd450e142451
Juha Kauppinen <juha.kauppinen@helsinki.fi>
2023-01-30 10:30:21 +0200

    add some tests

--- server/controllers/__test__/stations.test.js
+++ server/controllers/__test__/stations.test.js
@@ -2,8 +2,23 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('journey', () => {
-  it('should returns 200 if valid request', async () => {
+  it('should returns -200 Ok- if valid request', async () => {
     const response = await request(app).get('/api/stations');
     expect(response.statusCode).toEqual(200);
-  })
-})
+  });
+
+  it('returns -404 not found- if given station does not exist', async () => {
+    const response = await request(app).get('/api/stations/999');
+    expect(response.statusCode).toEqual(404);
+  });
+
+  it('returns -400 bad request- if given station id is not valid number', async () => {
+    const response = await request(app).get('/api/stations/null');
+    expect(response.statusCode).toEqual(400);
+  });
+
+  it('returns -400 bad request- if given station id is negative', async () => {
+    const response = await request(app).get('/api/stations/-123');
+    expect(response.statusCode).toEqual(400);
+  });
+});
