8944feb069d8cb6017ea9e43ea616c57c32d8758
Juha Kauppinen <scytile@gmail.com>
2023-02-05 16:11:13 +0200

    relative addresses for services

--- client/src/services/journeys.js
+++ client/src/services/journeys.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/api/journeys'
+const baseUrl = '/api/journeys'
 
 const getJourneys = (page, pageSize, field, order) => {
   const request = axios.get(`${baseUrl}?page=${page}&size=${pageSize}&field=${field}&order=${order}`)
--- client/src/services/stations.js
+++ client/src/services/stations.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/api/stations'
+const baseUrl = '/api/stations'
 
 /**
  * Get all stations
